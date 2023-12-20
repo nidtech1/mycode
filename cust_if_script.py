b6414570fa39e0429b5b59e52269447d6f782b79
nidtech1 <nidtech1036@gmail.com>
2023-12-20 18:23:55 +0000

    writing custom code

---
+++ cust_if_script.py
@@ -0,0 +1,18 @@
+Even if this is your first time coding, you should be able to stare and compare your way to some success. You can pick from one of the following ideas, or come up with your own:
+
+A program that prompts the user for network speed in Mbps, then grades the network performance (invent your own scale)
+A program that calculates postage rates
+http://www.stamps.com/usps/postage-rate-increase/
+A program that prompts the user for a value (1 through n), then returns a quick synopsis of a book or movie in that series.
+Harry Potter
+Hobbit & Lord of the Rings
+Chronicles of Narnia
+Indiana Jones
+A program that prompts a user for a numeric score, then returns the letter grade associated with that score.
+A (100 to 90)
+B (89 to 80)
+C (79 to 70)
+D (69 to 60)
+F (59 and below)
+A program that returns severity of a hurricane based upon wind speeds. Hurricane classifications are measured with the Saffir-Simpson Hurricane Winds Scale
+Or, write your own totally custom program using if, elif, and else.
